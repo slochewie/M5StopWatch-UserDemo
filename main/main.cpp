4e44e0a4e3639a5fe76cf414f52bb419a3883f75
Aaron Wilson <65009367+slochewie@users.noreply.github.com>
2026-05-31 06:59:15 -0700

    Add Counter app and remove unused demo apps

--- main/main.cpp
+++ main/main.cpp
@@ -17,30 +17,21 @@ using namespace smooth_ui_toolkit;
 
 extern "C" void app_main(void)
 {
-    // Setup logger
     mclog::set_level(mclog::level_info);
     mclog::set_time_format(mclog::time_format_unix_milliseconds);
 
-    // HAL init
     GetHAL().init();
 
-    // Setup ui hal
     ui_hal::on_delay([](uint32_t ms) { GetHAL().delay(ms); });
     ui_hal::on_get_tick([]() { return GetHAL().millis(); });
 
-    // Install apps
     GetMooncake().installApp(std::make_unique<AppLauncher>());
     GetMooncake().installApp(std::make_unique<AppAlarmClock>());
     GetMooncake().installApp(std::make_unique<AppWatchFace>());
     GetMooncake().installApp(std::make_unique<AppStopWatch>());
-    GetMooncake().installApp(std::make_unique<AppBadge>());
-    GetMooncake().installApp(std::make_unique<AppImu>());
-    GetMooncake().installApp(std::make_unique<AppFft>());
-    GetMooncake().installApp(std::make_unique<AppLuckyWheel>());
+    GetMooncake().installApp(std::make_unique<AppCounter>());
     GetMooncake().installApp(std::make_unique<AppSetup>());
-    // GetMooncake().installApp(std::make_unique<AppTemplate>());
 
-    // Main loop
     while (1) {
         GetHAL().feedTheDog();
         GetMooncake().update();
