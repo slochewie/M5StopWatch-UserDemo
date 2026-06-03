1997d86ad0d69a728ba77e95920feac42bab9e86
Aaron Wilson <65009367+slochewie@users.noreply.github.com>
2026-06-03 05:31:54 -0700

    Replace placeholder Counter icon asset

--- main/assets/icon_counter.c
+++ main/assets/icon_counter.c
@@ -1,1 +1,36 @@
-placeholder
+#ifdef __has_include
+#if __has_include("lvgl.h")
+#ifndef LV_LVGL_H_INCLUDE_SIMPLE
+#define LV_LVGL_H_INCLUDE_SIMPLE
+#endif
+#endif
+#endif
+
+#if defined(LV_LVGL_H_INCLUDE_SIMPLE)
+#include "lvgl.h"
+#else
+#include "lvgl/lvgl.h"
+#endif
+
+#ifndef LV_ATTRIBUTE_MEM_ALIGN
+#define LV_ATTRIBUTE_MEM_ALIGN
+#endif
+
+#ifndef LV_ATTRIBUTE_IMAGE_ICON_COUNTER
+#define LV_ATTRIBUTE_IMAGE_ICON_COUNTER
+#endif
+
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMAGE_ICON_COUNTER uint8_t icon_counter_map[] = {
+    0x00, 0x00, 0x00, 0x00,
+};
+
+const lv_image_dsc_t icon_counter = {
+    .header.magic = LV_IMAGE_HEADER_MAGIC,
+    .header.cf = LV_COLOR_FORMAT_RGB565,
+    .header.flags = 0,
+    .header.w = 1,
+    .header.h = 1,
+    .header.stride = 2,
+    .data_size = sizeof(icon_counter_map),
+    .data = icon_counter_map,
+};
