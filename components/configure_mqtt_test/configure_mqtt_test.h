5d0222094a9065bbfe6229160697271a5aaaa708
Aaron Wilson <65009367+slochewie@users.noreply.github.com>
2026-06-04 11:29:11 -0700

    Pass Wi-Fi settings to MQTT test helper

--- components/configure_mqtt_test/configure_mqtt_test.h
+++ components/configure_mqtt_test/configure_mqtt_test.h
@@ -4,7 +4,9 @@
 
 namespace configure_mqtt_test {
 
-std::string testConnection(const std::string& uri,
+std::string testConnection(const std::string& wifi_ssid,
+                           const std::string& wifi_password,
+                           const std::string& uri,
                            const std::string& username,
                            const std::string& password);
 
