a6eaee13232fe16ea6e965e7610466c592ef3857
Aaron Wilson <65009367+slochewie@users.noreply.github.com>
2026-06-03 17:22:14 -0700

    Expose counter publish function

--- components/counter_mqtt/counter_mqtt.h
+++ components/counter_mqtt/counter_mqtt.h
@@ -7,6 +7,7 @@ namespace counter_mqtt {
 void begin();
 bool isStarted();
 bool isConnected();
+bool publishCounterValue(int32_t value);
 bool takeLatestValue(int32_t& value);
 const char* statusText();
 
