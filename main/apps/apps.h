c1df08f59548d9913ee4049aff240d24c176c156
Aaron Wilson <65009367+slochewie@users.noreply.github.com>
2026-06-07 03:29:51 -0700

    Remove StopWatch app include

--- main/apps/apps.h
+++ main/apps/apps.h
@@ -7,7 +7,6 @@
 #include "app_template/app_template.h"
 #include "app_launcher/app_launcher.h"
 #include "app_watch_face/app_watch_face.h"
-#include "app_stopwatch/app_stopwatch.h"
 #include "app_counter/app_counter.h"
 #include "app_configure/app_configure.h"
 #include "app_setup/app_setup.h"
