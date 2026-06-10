ba662e2ac3f38fe5e442a163a4bd8562e3d696d3
Aaron Wilson <65009367+slochewie@users.noreply.github.com>
2026-06-10 10:16:21 -0700

    Route recovery through common services

--- components/counter_mqtt/counter_mqtt_recovery.cpp
+++ components/counter_mqtt/counter_mqtt_recovery.cpp
@@ -1,21 +1,14 @@
 #include "counter_mqtt.h"
 
-#include <esp_err.h>
-#include <esp_log.h>
-#include <esp_wifi.h>
+#include <apps/common/network/mqtt_service.h>
+#include <apps/common/network/wifi_service.h>
 
 namespace counter_mqtt {
 
 void recoverConnection()
 {
-    static constexpr const char* TAG = "CounterMQTT";
-
-    ESP_LOGI(TAG, "Wi-Fi recovery requested");
-
-    const esp_err_t err = esp_wifi_connect();
-    if (err != ESP_OK && err != ESP_ERR_WIFI_CONN && err != ESP_ERR_WIFI_NOT_STARTED) {
-        ESP_LOGW(TAG, "esp_wifi_connect failed: %s", esp_err_to_name(err));
-    }
+    common::wifi::recoverConnection();
+    common::mqtt::recoverConnection();
 }
 
 }  // namespace counter_mqtt
